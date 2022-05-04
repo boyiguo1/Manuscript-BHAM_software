dc71f6de8ee4ce38c459e52e70d67a8c091e25ea
Boyi Guo <boyiguo1992@gmail.com>
2022-05-04 13:56:19 -0500

    Organize manuscript structure

--- _targets.R
+++ _targets.R
@@ -7,18 +7,15 @@ lapply(list.files("./R", full.names = TRUE), source)
 ## tar_plan supports drake-style targets and also tar_target()
 tar_plan(
 
-# target = function_to_make(arg), ## drake style
 
-# tar_target(target2, function_to_make2(arg)) ## targets style
   # Manuscript --------------------------------------------------------------
   # * Child File Paths
   tar_files(child_rmds,
             c(
               "./Manuscript/01-intro.Rmd",
-              # "./Manuscript/02-method.Rmd",
-              # "./Manuscript/03-sim.Rmd",
-              # "./Manuscript/04-real_data.Rmd",
-              # "./Manuscript/05-conclusion.Rmd",
+              "./Manuscript/02-model.Rmd",
+              "./Manuscript/03-code.Rmd",
+              "./Manuscript/04-discussion.Rmd",
               "./Manuscript/references.bib"
             )
   ),
